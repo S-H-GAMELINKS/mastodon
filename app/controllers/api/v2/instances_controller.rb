123fae4b5fc8dfcf68f62d7b00ee328fec0825b2
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-05-26 23:25:37 +0900

    Introduce revision up

--- app/controllers/api/v2/instances_controller.rb
+++ app/controllers/api/v2/instances_controller.rb
@@ -2,7 +2,6 @@
 
 class Api::V2::InstancesController < Api::V1::InstancesController
   def show
-    cache_even_if_authenticated!
-    render_with_cache json: InstancePresenter.new, serializer: REST::InstanceSerializer, root: 'instance'
+    render json: InstancePresenter.new, serializer: REST::InstanceSerializer, root: 'instance'
   end
 end
---
+++ revision.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# .env.productionを読み込む
+env = File.read('.env.production')
+
+# 最新のコミットのハッシュを取得
+hash = `git show --format='%H' --no-patch`
+
+# 最新のコミットのハッシュをMASTODON_VERSION_SUFFIXに設定
+env = env.gsub!(/^MASTODON_VERSION_SUFFIX=.+$/, "MASTODON_VERSION_SUFFIX=#{hash}")
+
+# .env.productionへ置き換えたものを書き込む
+File.write('.env.production', env)
