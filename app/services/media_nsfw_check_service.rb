c1e005ae7514f7da71d0c5d67b88623e0d050e31
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-06-07 21:49:20 +0900

    Introduce Auto NSFW flag on with nsfw-rb

---
+++ app/services/media_nsfw_check_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'nsfw'
+
+class MediaNsfwCheckService < BaseService
+  def call(media_ids)
+    # チェックする画像を取得
+    medias = MediaAttachment.find(media_ids)
+
+    medias.map do |media|
+      ::NSFW::Image.unsafe?(media.file.path)
+    end.count(true).positive?
+  end
+end
