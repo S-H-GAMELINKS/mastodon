03490a1f8e668d195ba85e8ac7de60e9d3dd56b9
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-06-23 21:17:06 +0900

    Set entry point and scss build config

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,8 @@
 const path    = require("path")
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require("webpack")
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts')
 
 module.exports = {
   mode: "production",
@@ -12,19 +14,51 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ],
   },
   entry: {
-    application: "./app/javascript/application.js"
+    admin: './app/javascript/packs/admin.jsx', 
+    application: [
+      "./app/javascript/packs/application.js",
+      "./app/javascript/styles/application.scss"
+    ],
+    error: "./app/javascript/packs/error.js",
+    mailer: [
+      "./app/javascript/packs/mailer.js",
+      "./app/javascript/styles/mailer.scss"
+    ],
+    public: "./app/javascript/packs/public.js",
+    share: "./app/javascript/packs/share.js",
+    sign_up: "./app/javascript/packs/sign_up.js",
+    two_factor_authentication: "./app/javascript/packs/two_factor_authentication.js",
+    creatodon: "./app/javascript/styles/application_creatodon.scss",
+    kohaku: "./app/javascript/styles/application_kohaku.scss",
+    mean_of_amber: "./app/javascript/styles/application_mean_of_amber.scss",
+    meteor_trail: "./app/javascript/styles/application_meteor_trail.scss",
+    mirroring: "./app/javascript/styles/application_mirroring.scss",
+    mizuki: "./app/javascript/styles/application_mizuki.scss",
+    orochi: "./app/javascript/styles/application_orochi.scss",
+    twitter: "./app/javascript/styles/application_twitter.scss",
+    contrast: "./app/javascript/styles/contrast.scss",
+    "mastodon-light": "./app/javascript/styles/mastodon-light.scss" 
   },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.css'],
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
   ]
 }
