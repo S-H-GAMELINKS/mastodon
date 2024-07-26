5116ab87c6e9ee4e2e5a2231dba2ade9cee4344f
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-07-26 23:43:31 +0900

    Fix typo for instance variable name

--- app/services/delete_status_service.rb
+++ app/services/delete_status_service.rb
@@ -5,7 +5,7 @@ class DeleteStatusService < BaseService
   def call(status_id)
     @status = Status.find(status_id)
 
-    @ftatus.discard_with_reblogs
+    @status.discard_with_reblogs
     StatusPin.find_by(status: @status)&.destroy
     @status.account.statuses_count = @status.account.statuses_count - 1
 
